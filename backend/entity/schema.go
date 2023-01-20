b0653fad8932eae36186710d4b5c2a66dcb8985f
AonNPT080245 <nptkkp.1048@gmail.com>
2023-01-20 23:36:49 +0700

    added entity of trainer subsystem - close #16

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -30,3 +30,53 @@ type Blog struct {
 	TagID *uint
 	Tag   Tag
 }
+
+// -------------------------------------------<< ระบบจัดการเทรนอร์ >>------------------------------------
+type FormOfWork struct {
+	gorm.Model
+	Name    string
+	Trainer []Trainer `gorm:"foreignKey:FormOfWorkID"`
+}
+type Status struct {
+	gorm.Model
+	Name    string
+	Trainer []Trainer `gorm:"foreignKey:StatusID"`
+}
+
+type Education struct {
+	gorm.Model
+	EducationLevel string
+	Trainer        []Trainer `gorm:"foreignKey:EducationID"`
+}
+type Religion struct {
+	gorm.Model
+	Name    string
+	Trainer []Trainer `gorm:"foreignKey:ReligionID"`
+}
+
+type Trainer struct {
+	gorm.Model
+	Name       string
+	University string
+	Gpax       float32
+	Gender     string
+	Age        int
+	Address    string
+	Email      string
+	UserName   string `gorm:"uniqueIndex"`
+	Password   string `gorm:"uniqueIndex"`
+
+	FormOfWorkID *uint
+	FormOfWork   FormOfWork
+
+	StatusID *uint
+	Status   Status
+
+	EducationID *uint
+	Education   Education
+
+	ReligionID *uint
+	Religion   Religion
+}
+
+// -------------------------------------------<<  >>------------------------------------
