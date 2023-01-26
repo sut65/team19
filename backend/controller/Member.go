df3dd8ab0109000d818dd8bd14970b26f9d18f84
Hot <hotgabpom@gmail.com>
2023-01-26 11:50:34 +0700

    create controller register

--- backend/controller/Member.go
+++ backend/controller/Member.go
@@ -5,28 +5,67 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/sut65/team19/entity"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // POST /member
 func CreateMember(c *gin.Context) {
 	var member entity.Member
+	var status entity.Status
+	var gender entity.Gender
+	var religion entity.Religion
+
 	if err := c.ShouldBindJSON(&member); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if err := entity.DB().Create(&member).Error; err != nil {
+	// ค้นหา status ด้วย id
+	if tx := entity.DB().Where("id = ?", member.StatusID).First(&status); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "status not found"})
+		return
+	}
+
+	// ค้นหา gender ด้วย id
+	if tx := entity.DB().Where("id = ?", member.GenderID).First(&gender); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "gender not found"})
+		return
+	}
+
+	// ค้นหา religion ด้วย id
+	if tx := entity.DB().Where("id = ?", member.ReligionID).First(&religion); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "religion not found"})
+		return
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(member.Password), 14)
+
+	// สร้าง Member
+	fif := entity.Member{
+		Gender:      gender,
+		Status:      status,
+		Religion:    religion,
+		ProfileUser: member.ProfileUser,
+		Firstname:   member.Firstname,
+		Lastname:    member.Lastname,
+		Email:       member.Email,
+		Password:    string(password),
+	}
+
+	// บันทึก
+	if err := entity.DB().Create(&fif).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"data": member})
+	c.JSON(http.StatusCreated, gin.H{"data": fif})
 }
 
 // GET /member/:id
 func GetMember(c *gin.Context) {
 	var member entity.Member
 	id := c.Param("id")
-	if tx := entity.DB().Where("id = ?", id).First(&member); tx.RowsAffected == 0 {
+
+	if tx := entity.DB().Preload("Gender").Preload("Status").Preload("Religion").Where("id = ?", id).First(&member); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "member not found"})
 		return
 	}
@@ -37,7 +76,7 @@ func GetMember(c *gin.Context) {
 // GET /members
 func ListMembers(c *gin.Context) {
 	var members []entity.Member
-	if err := entity.DB().Raw("SELECT * FROM members").Scan(&members).Error; err != nil {
+	if err := entity.DB().Preload("Gender").Preload("Status").Preload("Religion").Raw("SELECT * FROM members").Find(&members).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -59,20 +98,26 @@ func DeleteMember(c *gin.Context) {
 // PATCH /member
 func UpdateMember(c *gin.Context) {
 	var member entity.Member
+	var new_member entity.Member
+
 	if err := c.ShouldBindJSON(&member); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if tx := entity.DB().Where("id = ?", member.ID).First(&member); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "member not found"})
-		return
-	}
+	new_member.Gender = member.Gender
+	new_member.Status = member.Status
+	new_member.Religion = member.Religion
+	new_member.ProfileUser = member.ProfileUser
+	new_member.Firstname = member.Firstname
+	new_member.Lastname = member.Lastname
+	new_member.Email = member.Email
+	new_member.Password = member.Password
 
-	if err := entity.DB().Save(&member).Error; err != nil {
+	if err := entity.DB().Where("id = ?", member.ID).Updates(&new_member).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": member})
+	c.JSON(http.StatusOK, gin.H{"data": new_member})
 }
