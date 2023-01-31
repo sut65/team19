ed1c687aeba5037a6d381ba961a09fe6104b59f2
Thanaporn4226 <bambt4226@gmail.com>
2023-01-31 17:14:37 +0700

    edit controller course detail and edit main.go - close #26

---
+++ backend/controller/CourseDetail/CourseDetails.go
@@ -0,0 +1,137 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team19/entity"
+)
+
+// POST /course_detail
+func CreateCourseDetail(c *gin.Context) {
+	var course_detail entity.CourseDetail
+	var price entity.Price
+	var admin entity.Admin
+	var description entity.Description
+
+	if err := c.ShouldBindJSON(&course_detail); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ค้นหา admin ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.AdminID).First(&admin); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "admin not found"})
+		return
+	}
+
+	// ค้นหา price ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.PriceID).First(&price); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "price not found"})
+		return
+	}
+
+	// ค้นหา description ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.DescriptionID).First(&description); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "description not found"})
+		return
+	}
+
+	// สร้าง CourseDetail
+	cd := entity.CourseDetail{
+		CourseName:  course_detail.CourseName,
+		CoverPage:   course_detail.CoverPage,
+		Description: description,
+		Admin:       admin,
+		Price:       price,
+	}
+
+	// บันทึก
+	if err := entity.DB().Create(&cd).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": cd})
+}
+
+// GET /course_detail/:id
+func GetCourseDetail(c *gin.Context) {
+	var course_detail entity.CourseDetail
+	id := c.Param("id")
+	if tx := entity.DB().Preload("Price").Preload("Description").Preload("Admin").Where("id = ?", id).First(&course_detail); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "course_detail not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": course_detail})
+}
+
+// GET /course_details
+func ListCourseDetails(c *gin.Context) {
+	var course_details []entity.CourseDetail
+
+	if err := entity.DB().Preload("Price").Preload("Description").Preload("Admin").Raw("SELECT * FROM course_details").Find(&course_details).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": course_details})
+}
+
+// DELETE /course_detail/:id
+func DeleteCourseDetail(c *gin.Context) {
+	id := c.Param("id")
+
+	if tx := entity.DB().Exec("DELETE FROM course_details WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "course_detail not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// PATCH /course_detail
+func UpdateCourseDetail(c *gin.Context) {
+	var course_detail entity.CourseDetail
+	var description entity.Description
+	var price entity.Price
+	var admin entity.Admin
+
+	if err := c.ShouldBindJSON(&course_detail); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// ค้นหา admin ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.AdminID).First(&admin); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	// ค้นหา description ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.DescriptionID).First(&description); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "category not found"})
+		return
+	}
+
+	// ค้นหา price ด้วย id
+	if tx := entity.DB().Where("id = ?", course_detail.PriceID).First(&price); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "tag not found"})
+		return
+	}
+
+	update := entity.CourseDetail{
+		CourseName:  course_detail.CourseName,
+		CoverPage:   course_detail.CoverPage,
+		Description: description,
+		Admin:       admin,
+		Price:       price,
+	}
+
+	if err := entity.DB().Where("id = ?", course_detail.ID).Updates(&update).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": course_detail})
+}
---
+++ backend/controller/CourseDetail/Description.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team19/entity"
+	"gorm.io/gorm/clause"
+)
+
+// GET /description/:id
+func GetDescription(c *gin.Context) {
+	var description entity.Description
+	id := c.Param("id")
+
+	if tx := entity.DB().Preload(clause.Associations).Preload("course_details."+clause.Associations).Where("id = ?", id).First(&description); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "description not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": description})
+}
+
+// GET /description
+func ListDescriptions(c *gin.Context) {
+	var descriptions []entity.Description
+	if err := entity.DB().Preload(clause.Associations).Preload("course_details." + clause.Associations).Raw("SELECT * FROM descriptions").Find(&descriptions).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": descriptions})
+}
